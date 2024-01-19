4bd760863ac80ea6c75beffd87f2c30b227a9f16
sanjay-gunagi <sanjay.gunagi@jobvite-inc.com>
2024-01-19 12:00:24 +0530

    Code splitting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Suspense, lazy } from "react";
 import './app.scss';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
 
 const Test = lazy(() => import('./Test'));
 
 const  App = () => {
    return ( <div className="app">
         <h1 style={{color: 'green'}}>App Hi Hello Hi Check Test Tesstttt</h1>
-       <Suspense fallback={<div>Loading..</div>}>
+{/*          <Suspense fallback={<div>Loading..</div>}>
             <Test/>
-        </Suspense>
+        </Suspense> */}
+
+        <BrowserRouter>
+
+        <Link to='/test'>Test</Link>
+
+        <Routes>
+            <Route path="/test" element={ <Suspense fallback={<div>Loading..</div>}>
+            <Test/>
+        </Suspense>} />
+        </Routes>
+        
+        </BrowserRouter>
+      
     </div>)
 }
 
